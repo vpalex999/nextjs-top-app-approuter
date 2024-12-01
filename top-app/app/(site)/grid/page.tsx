69f383859e0a63215ddb26458a8922e459b7c919
vpalex999 <vpalex@mail.ru>
2024-12-01 10:56:55 +0500

    stage-10.2

---
+++ top-app/app/(site)/grid/page.tsx
@@ -0,0 +1,25 @@
+import cn from "classnames";
+import styles from "./Grid.module.css";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Grid CSS",
+};
+
+export default function Grid() {
+  return (
+    <section className={cn(styles.section)}>
+      <div className={cn(styles.container)}>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+        <div>6</div>
+        <div>7</div>
+        <div>8</div>
+        <div>9</div>
+      </div>
+    </section>
+  );
+}
