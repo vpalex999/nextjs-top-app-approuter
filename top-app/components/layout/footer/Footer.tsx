e3ba2f526928763b943aa16016df43386244b2f2
vpalex999 <vpalex@mail.ru>
2024-12-07 13:59:15 +0500

    stage-10.8

--- top-app/components/layout/footer/Footer.tsx
+++ top-app/components/layout/footer/Footer.tsx
@@ -1,13 +1,24 @@
 import { Paragraph } from "@/components/Paragraph";
 import { DetailedHTMLProps, HTMLAttributes } from "react";
+import cn from "classnames";
+import styles from "./Footer.module.css";
+import { format } from "date-fns";
 
 interface FooterProps
   extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {}
 
-export const Footer = (props: FooterProps) => {
+export const Footer = ({ className, ...props }: FooterProps) => {
   return (
-    <div {...props}>
-      <Paragraph>Footer</Paragraph>
-    </div>
+    <footer className={cn(className, styles.footer)} {...props}>
+      <Paragraph size="l">
+        OwlTop 2020- {format(new Date(), "yyyy")} Все права защищены
+      </Paragraph>
+      <a href="#" target="_blank">
+        <Paragraph size="l">Пользовательское соглашение</Paragraph>
+      </a>
+      <a href="#" target="_blank">
+        <Paragraph size="l">Политика конфиденциальности</Paragraph>
+      </a>
+    </footer>
   );
 };
