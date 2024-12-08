e2a46eac1991953ace0499337f266877d408eb48
vpalex999 <vpalex@mail.ru>
2024-12-08 12:13:49 +0500

    stage-11.3

--- top-app/app/(site)/page.tsx
+++ top-app/app/(site)/page.tsx
@@ -1,6 +1,7 @@
 import { Paragraph } from "@/components";
 
 export default function Home() {
+  console.log('I am main page!')
   return (
     <main>
       <Paragraph>Главная страница</Paragraph>
