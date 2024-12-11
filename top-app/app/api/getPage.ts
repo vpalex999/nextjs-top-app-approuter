e4f1da065d2bea4e11a56ea876993f57339071b4
vpalex999 <vpalex@mail.ru>
2024-12-11 20:33:08 +0500

    stage-11.9

--- top-app/app/api/getPage.ts
+++ top-app/app/api/getPage.ts
@@ -3,6 +3,7 @@ import { API } from "../api";
 export const getPage = async (alias: string): Promise<TopPageModel | null> => {
   const res = await fetch(API.topPage.byAlias + `/${alias}`, {
     method: "GET",
+    next: { revalidate: 10 },
   });
 
   if (res.ok) {
